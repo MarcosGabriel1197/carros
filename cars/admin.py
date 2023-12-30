49a3ba76c666b77d9ad619b2a9576d900188ae96
ippo7v <marcosgabriel1197@gmail.com>
2023-12-30 16:23:14 -0300

    first commit

---
+++ cars/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from cars.models import Car, Brand
+
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model', 'brand')
+
+
+class Brandadmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Brand, Brandadmin)
+admin.site.register(Car, CarAdmin)
+
